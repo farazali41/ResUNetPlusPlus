97efdd5398824cfee81f4eee501f6ca4e8f5270c
farazali41 <farazali41@yahoo.com>
2020-10-11 20:47:40 +0500

    initial commit

---
+++ run_sample.py
@@ -0,0 +1,149 @@
+import os
+import numpy as np
+import cv2
+import random
+from glob import glob
+import tensorflow as tf
+from tensorflow.keras.metrics import Precision, Recall, MeanIoU
+from tensorflow.keras.optimizers import Adam, Nadam, SGD
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+
+from data_generator import DataGen
+from unet import Unet
+from resunet import ResUnet
+from m_resunet import ResUnetPlusPlus
+from metrics import dice_coef, dice_loss
+
+def iterSample(iterable, samplesize):
+    results = []
+
+    for i, v in enumerate(iterable):
+        r = random.randint(0, i)
+        if r < samplesize:
+            if i < samplesize:
+                results.insert(r, v) # add first samplesize items in random order
+            else:
+                results[r] = v # at a decreasing rate, replace random items
+
+    if len(results) < samplesize:
+        raise ValueError("Sample larger than population.")
+
+    return results
+
+
+if __name__ == "__main__":
+    ## Path
+    file_path = "files/"
+    model_path1 = "files/resunetplusplus1.h5"
+    model_path2 = "files/resunetplusplus2.h5"
+    model_path3 = "files/resunetplusplus3.h5"
+
+    ## Create files folder
+    try:
+        os.mkdir("files")
+    except:
+        pass
+
+    train_path = "new_data/Kvasir-SEG/train/"
+    valid_path = "new_data/Kvasir-SEG/valid/"
+
+    ## Training
+    train_image_paths = glob(os.path.join(train_path, "images", "*"))
+    train_mask_paths = glob(os.path.join(train_path, "masks", "*"))
+    train_image_paths.sort()
+    train_mask_paths.sort()
+
+    # train_image_paths = train_image_paths[:2000]
+    # train_mask_paths = train_mask_paths[:2000]
+
+    ## Validation
+    valid_image_paths = glob(os.path.join(valid_path, "images", "*"))
+    valid_mask_paths = glob(os.path.join(valid_path, "masks", "*"))
+    valid_image_paths.sort()
+    valid_mask_paths.sort()
+
+    ## Parameters
+    image_size = 256
+    batch_size = 8
+    lr = 1e-4
+    epochs = 2
+
+    train_steps = len(train_image_paths)//batch_size
+    valid_steps = len(valid_image_paths)//batch_size
+
+    ## Generator
+    train_gen = DataGen(image_size, train_image_paths, train_mask_paths, batch_size=batch_size)
+    valid_gen = DataGen(image_size, valid_image_paths, valid_mask_paths, batch_size=batch_size)
+
+    train_gen1 = iterSample(iterable = train_gen, samplesize = 16000)
+    train_gen2 = iterSample(iterable = train_gen, samplesize = 16000)
+    train_gen3 = iterSample(iterable = train_gen, samplesize = 16000)
+
+    ## Unet
+    # arch = Unet(input_size=image_size)
+    # model = arch.build_model()
+
+    ## ResUnet
+    # arch = ResUnet(input_size=image_size)
+    # model = arch.build_model()
+
+    ## ResUnet++
+    arch = ResUnetPlusPlus(input_size=image_size)
+    model1 = arch.build_model()
+
+    optimizer = Nadam(lr)
+    metrics = [Recall(), Precision(), dice_coef, MeanIoU(num_classes=2)]
+    model1.compile(loss=dice_loss, optimizer=optimizer, metrics=metrics)
+
+    csv_logger = CSVLogger(f"{file_path}resunet1_{batch_size}.csv", append=False)
+    checkpoint = ModelCheckpoint(model_path1, verbose=1, save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-6, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=False)
+    callbacks = [csv_logger, checkpoint, reduce_lr, early_stopping]
+
+    model1.fit_generator(train_gen,
+            validation_data=valid_gen,
+            steps_per_epoch=train_steps,
+            validation_steps=valid_steps,
+            epochs=epochs,
+            callbacks=callbacks)
+
+    arch = ResUnetPlusPlus(input_size=image_size)
+    model2 = arch.build_model()
+
+    optimizer = Nadam(lr)
+    metrics = [Recall(), Precision(), dice_coef, MeanIoU(num_classes=2)]
+    model2.compile(loss=dice_loss, optimizer=optimizer, metrics=metrics)
+
+    csv_logger = CSVLogger(f"{file_path}resunet2_{batch_size}.csv", append=False)
+    checkpoint = ModelCheckpoint(model_path2, verbose=1, save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-6, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=False)
+    callbacks = [csv_logger, checkpoint, reduce_lr, early_stopping]
+
+    model2.fit_generator(train_gen,
+            validation_data=valid_gen,
+            steps_per_epoch=train_steps,
+            validation_steps=valid_steps,
+            epochs=epochs,
+            callbacks=callbacks)
+
+    arch = ResUnetPlusPlus(input_size=image_size)
+    model3 = arch.build_model()
+
+    optimizer = Nadam(lr)
+    metrics = [Recall(), Precision(), dice_coef, MeanIoU(num_classes=2)]
+    model3.compile(loss=dice_loss, optimizer=optimizer, metrics=metrics)
+
+    csv_logger = CSVLogger(f"{file_path}resunet3_{batch_size}.csv", append=False)
+    checkpoint = ModelCheckpoint(model_path3, verbose=1, save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-6, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=False)
+    callbacks = [csv_logger, checkpoint, reduce_lr, early_stopping]
+
+    model3.fit_generator(train_gen,
+            validation_data=valid_gen,
+            steps_per_epoch=train_steps,
+            validation_steps=valid_steps,
+            epochs=epochs,
+            callbacks=callbacks)
